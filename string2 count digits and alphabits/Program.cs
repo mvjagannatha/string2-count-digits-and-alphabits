f43366897f86aa564ead18ef12eb30178e7e6290
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 19:54:44 +0530

    Add project files.

---
+++ string2 count digits and alphabits/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class program
+{
+    public static void Main()
+    {
+        string str;
+        int alp, digit,i, l;
+        alp = digit = i = 0;
+             
+        Console.Write("Input the string : ");
+        str = Console.ReadLine();
+        l = str.Length;
+
+        
+        while (i < l)
+        {
+            if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            {
+                alp++;
+            }
+            else if (str[i] >= '0' && str[i] <= '9')
+            {
+                digit++;
+            }
+            
+
+            i++;
+        }
+
+        Console.Write("Number of Alphabets in the string is : {0}\n", alp);
+        Console.Write("Number of Digits in the string is : {0}\n", digit);
+        
+    }
+}
